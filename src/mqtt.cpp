1be3ad15de2c08fccb3ede3cce9e6ff735a7c7a5
Fairesoimeme <akila@faire-ca-soi-meme.fr>
2025-06-10 15:16:17 +0200

    Fix MQTT deconnection

--- src/mqtt.cpp
+++ src/mqtt.cpp
@@ -9,28 +9,21 @@ extern AsyncMqttClient mqttClient;
 extern ConfigGeneralStruct ConfigGeneral;
 extern ConfigSettingsStruct ConfigSettings;
 
+extern unsigned long lastConnectionTest;
+extern const unsigned long CONNECTION_TEST_INTERVAL;
+extern bool connectionTestPending ;
+extern uint16_t lastTestPacketId ;
+extern bool reallyConnected ;
+
 void mqttPublish(String IEEE, String cluster, String attribute, String type, String value)
 {
-    String tmpvalue;
-    if (ConfigGeneral.HAMQTT)
-    { 
-        tmpvalue = "{\"value_"+cluster+"_"+attribute+"\":";
-        if (type=="string")
-        {
-            tmpvalue += "\""+value+"\"";
-        }else if (type=="numeric")
-        {
-            tmpvalue += String(strtol(value.c_str(), NULL, 16));
-        }
-        
-        tmpvalue +="}";
-        String topic = ConfigGeneral.headerMQTT+ IEEE+"_"+cluster+"_"+attribute+"/state";
-        mqttClient.publish(topic.c_str(), 0, true, tmpvalue.c_str());
-    }else if (ConfigGeneral.TBMQTT)
+    unsigned long now = millis();
+    if (mqttClient.connected()) 
     {
-        tmpvalue ="{\""+IEEE+"\":[";
-        tmpvalue +="{";
-            tmpvalue+="\""+cluster+"_"+attribute+"\" : ";
+        String tmpvalue;
+        if (ConfigGeneral.HAMQTT)
+        { 
+            tmpvalue = "{\"value_"+cluster+"_"+attribute+"\":";
             if (type=="string")
             {
                 tmpvalue += "\""+value+"\"";
@@ -38,25 +31,53 @@ void mqttPublish(String IEEE, String cluster, String attribute, String type, Str
             {
                 tmpvalue += String(strtol(value.c_str(), NULL, 16));
             }
-        tmpvalue +="}";
-        tmpvalue +="]}";
-        mqttClient.publish(ConfigGeneral.headerMQTT, 0, true, tmpvalue.c_str());
-    }else if (ConfigGeneral.customMQTT)
-    {
-        tmpvalue = ConfigGeneral.customMQTTJson;
-        tmpvalue.replace("<IEEE>",IEEE);
-        tmpvalue.replace("<cluster>",cluster);
-        tmpvalue.replace("<attribute>",attribute);
-        if (type=="string")
-        {
-            tmpvalue.replace("<value>","\""+value+"\"");
-        }else if (type=="numeric")
-        {
-            tmpvalue.replace("<value>",String(strtol(value.c_str(), NULL, 16)));
-        }
+            
+            tmpvalue +="}";
+            String topic = ConfigGeneral.headerMQTT+ IEEE+"_"+cluster+"_"+attribute+"/state";
+            mqttClient.publish(topic.c_str(), 1, false, tmpvalue.c_str());
+            
 
-        mqttClient.publish(ConfigGeneral.headerMQTT, 0, true, tmpvalue.c_str());
+        }else if (ConfigGeneral.TBMQTT)
+        {
+            tmpvalue ="{\""+IEEE+"\":[";
+            tmpvalue +="{";
+                tmpvalue+="\""+cluster+"_"+attribute+"\" : ";
+                if (type=="string")
+                {
+                    tmpvalue += "\""+value+"\"";
+                }else if (type=="numeric")
+                {
+                    tmpvalue += String(strtol(value.c_str(), NULL, 16));
+                }
+            tmpvalue +="}";
+            tmpvalue +="]}";
+            mqttClient.publish(ConfigGeneral.headerMQTT, 1, false, tmpvalue.c_str());
+        }else if (ConfigGeneral.customMQTT)
+        {
+            tmpvalue = ConfigGeneral.customMQTTJson;
+            tmpvalue.replace("<IEEE>",IEEE);
+            tmpvalue.replace("<cluster>",cluster);
+            tmpvalue.replace("<attribute>",attribute);
+            if (type=="string")
+            {
+                tmpvalue.replace("<value>","\""+value+"\"");
+            }else if (type=="numeric")
+            {
+                tmpvalue.replace("<value>",String(strtol(value.c_str(), NULL, 16)));
+            }
+
+            mqttClient.publish(ConfigGeneral.headerMQTT, 1, false, tmpvalue.c_str());
+        }
+        vTaskDelay(10);
     }
-    vTaskDelay(10);
+
+    // Timeout du test de connexion
+    
+    int16_t time = (now - lastConnectionTest);
+    if (time > 10000) 
+    {
+        // Forcer la reconnexion
+        mqttClient.disconnect(true);
+    }
     
 }
