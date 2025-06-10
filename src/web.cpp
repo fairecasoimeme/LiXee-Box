99fb42622b21d1aff94e419190a0d2ed22884d7c
Fairesoimeme <akila@faire-ca-soi-meme.fr>
2025-06-10 15:17:22 +0200

    Fix reboot issue

--- src/web.cpp
+++ src/web.cpp
@@ -109,6 +109,7 @@ const char HTTP_HEADER[] PROGMEM =
         "border-radius: 1rem;"
         "box-shadow: 0 4px 6px rgba(0,0,0,0.05);"
       "}"
+      
     "</style>"
      "</head>";
 
@@ -143,6 +144,10 @@ const char HTTP_HEADERGRAPH[] PROGMEM =
       ".link.active {"
         "text-decoration: underline !important;"
       "}"
+      "#power_gauge_global svg,#power_gauge_global2 svg,#power_gauge_global3 svg,#power_gauge_prod svg {"
+        "height: auto !important;"
+      "}"
+      "#donut-chart svg{text-align:center;width:100%;margin-top:-50px;height:250px;}"
     "</style>"
     "</head>";
 
@@ -153,7 +158,7 @@ const char HTTP_MENU[] PROGMEM =
    "<div class='container-fluid' style=''>"
    "<button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNavDropdown' aria-controls='navbarNavDropdown' aria-expanded='false' aria-label='Toggle navigation'>"
    "<span class='navbar-toggler-icon'></span>"
-   "<div class='AboutMaj' style='display: inline-block; width: 8px;height: 8px; background-color: red; margin-left: 4px; vertical-align: middle;border-radius: 50%;  '></div>"
+   "<div class='AboutMaj' style='display:none; width: 8px;height: 8px; background-color: red; margin-left: 4px; vertical-align: middle;border-radius: 50%;  '></div>"
    "</button>"
    "<a class='navbar-brand p-0 me-0 me-lg-2' href='/' style='margin-right:0px;'>"
    "  <div style='display:block-inline;float:left;'><img width='70px' src='web/img/logo.png'> </div>"
@@ -322,7 +327,7 @@ const char HTTP_MENU[] PROGMEM =
    "  <path d='M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286m1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94'/>"
    "</svg>"
    " About"
-   "<div class='AboutMaj' style='display: inline-block; width: 8px;height: 8px; background-color: red; margin-left: 4px; vertical-align: middle;border-radius: 50%;  '></div>"
+   "<div class='AboutMaj' style='display:none; width: 8px;height: 8px; background-color: red; margin-left: 4px; vertical-align: middle;border-radius: 50%;  '></div>"
    "</a>"
    "<div class='dropdown-menu'>"
    "<a class='dropdown-item' href='/help'>"
@@ -338,7 +343,7 @@ const char HTTP_MENU[] PROGMEM =
     "  <path d='M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z'/>"
     "</svg>"
    " Update"
-   "<div class='AboutMaj' style='display: inline-block; width: 8px;height: 8px; background-color: red; margin-left: 4px; vertical-align: middle;border-radius: 50%;  '></div>"
+   "<div class='AboutMaj' style='display: none; width: 8px;height: 8px; background-color: red; margin-left: 4px; vertical-align: middle;border-radius: 50%;  '></div>"
    "</a>"
    "</div>"
    "</li>"
@@ -2141,7 +2146,7 @@ const char HTTP_FOOTER[] PROGMEM = R"(
           {
             $(".AboutMaj").hide();
           }else{
-            $(".AboutMaj").show();
+            $(".AboutMaj").css('display', 'inline-block');
           }
         });
       }
@@ -3866,6 +3871,7 @@ void handleStatusEnergy(AsyncWebServerRequest *request)
                 }
               });
             });
+
           </script>)";
   result+=footer();
   result += F("</html>");
@@ -3918,7 +3924,7 @@ void handleStatusEnergy(AsyncWebServerRequest *request)
   {
     if ((ConfigGeneral.LinkyMode == 2 ) || (ConfigGeneral.LinkyMode == 3 ) || (ConfigGeneral.LinkyMode == 7 ))
     {
-      powerGauge=F("<div class='col-md-8'>");
+      powerGauge=F("<div class='col-md-4'>");
             powerGauge +=F("<div class='card p-4'>");
               powerGauge +=F("<h5 class='card-title' style=''>Energy gauge</h5>");
               powerGauge +=F("<div class='card-body' style='min-height:270px;'>");
@@ -3997,7 +4003,7 @@ void handleStatusEnergy(AsyncWebServerRequest *request)
             if (strcmp(ConfigGeneral.Production,"") != 0 )
             {
               powerGauge += F("<div class='col-12 col-sm-12 col-lg-6 mb-3' style='text-align:center;'>");
-                powerGauge +=F("<h5>Phase 1</h5>");
+                powerGauge +=F("<h5>Consumption</h5>");
                 powerGauge +=F("<div id='power_gauge_global' class='w-100' ></div>");
               powerGauge +=F("</div>");
               powerGauge += F("<div class='col-12 col-sm-12 col-lg-6 mb-3' style='text-align:center;'>");
@@ -4006,7 +4012,7 @@ void handleStatusEnergy(AsyncWebServerRequest *request)
               powerGauge +=F("</div>");
             }else{
               powerGauge += F("<div class='col-12 col-sm-12 col-lg-12 mb-3' style='text-align:center;'>");
-                powerGauge +=F("<h5>Phase 1</h5>");
+                powerGauge +=F("<h5>Consumption</h5>");
                 powerGauge +=F("<div id='power_gauge_global' class='w-100' ></div>");
               powerGauge +=F("</div>");
             }
@@ -5566,11 +5572,11 @@ void handleReboot(AsyncWebServerRequest *request)
   result = result + F("</body>");
   result+=footer();
   result += F("</html>");
+   executeReboot=true;
   AsyncWebServerResponse *response = request->beginResponse(303);
   response->addHeader(F("Location"), F("/"));
   request->send(response);
 
-  hard_restart();
 }
 
 /*static void ensureDirs(const String &fullPath)
